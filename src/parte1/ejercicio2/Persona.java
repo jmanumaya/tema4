545408b865604185431707df31e43b3bcde0b404
[name] <[jm.maya@iesnervion.es]>
2025-02-05 10:09:23 +0100

    no acabado

--- src/parte1/ejercicio2/Persona.java
+++ src/parte1/ejercicio2/Persona.java
@@ -2,15 +2,32 @@ package parte1.ejercicio2;
 
 public class Persona {
 	
-	String dni;
-	String nombre;
-	String apellidos;
-	int edad;
+	private final String dni;
+	private String nombre;
+	private String apellidos;
+	private int edad;
 	
-	public Persona (String dni, String nombre, String apellidos, int edad) {
+	public Persona(String dni, String nombre, String apellidos, int edad) {
 		this.dni = dni;
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.edad = edad;
 	}
+	
+	public String getDni(){
+		return dni;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 }
