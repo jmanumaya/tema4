c5ad5a2c63b7053c44d26a227ec673f441141ee7
[name] <[jm.maya@iesnervion.es]>
2025-02-12 11:52:21 +0100

    Falta añadir mas metodos

--- src/parte2/ejercicio1/Principal.java
+++ src/parte2/ejercicio1/Principal.java
@@ -3,7 +3,19 @@ package parte2.ejercicio1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		CuentaCorriente cuenta1 = new CuentaCorriente("12345678P", "Jose Manuel", 2000, "española");
+		CuentaCorriente cuenta2 = new CuentaCorriente("87654321H", "Pepe", 3000);
+		
+		System.out.println(cuenta1);
+		System.out.println();
+		System.out.println(cuenta2);
+		
+		cuenta2.setNacionalidad("extranjera");
+		
+		System.out.println("\nNueva Nacionalidad de " + cuenta2.getNombre() + ": " + cuenta2.getNacionalidad());
+		
+		
 
 	}
 
