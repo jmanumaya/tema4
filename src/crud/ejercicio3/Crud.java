bc115b50dcdd720f7a1053ed50178a06f2f6188d
[name] <[jm.maya@iesnervion.es]>
2025-02-24 13:31:31 +0100

    En proceso

---
+++ src/crud/ejercicio3/Crud.java
@@ -0,0 +1,17 @@
+package crud.ejercicio3;
+
+import java.util.ArrayList;
+
+public class Crud {
+
+	ArrayList<Pizza> pizzas = new ArrayList<>();
+	
+	public ArrayList<Pizza> listadoPizzas(){
+		
+		return pizzas;
+	}
+	
+	public boolean nuevoPedido(int codigo, String tamaño, String tipo) {
+		
+	}
+}
---
+++ src/crud/ejercicio3/Principal.java
@@ -0,0 +1,84 @@
+package crud.ejercicio3;
+
+import java.util.Scanner;
+
+import crud.ejercicio2.Articulo;
+import crud.ejercicio2.Gestimal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Creo el escaner
+		Scanner sc = new Scanner(System.in);
+				
+		// Me creo un objero Gestimal para poder trabajar con sus metodos
+		Crud crud = new Crud(); // ¿? <-- Preguntar en clase ¿Lo dejo asi y así no tengo que poner static en cada metodo de la clase para acceder a el o alreves?
+		
+		// Variable que va a contener la opcion del menu elegida
+		String opcion = "";
+		
+		// Variable para almacenar el nombre dado en cada caso del articulo.
+		int codigo = 1;
+		
+		// Variable que va a ir conteniendo el precio de cada articulo en cada caso
+		String tamaño = "";
+		
+		// Variable que va a ir conteniendo la entrada de mercancia
+		String tipo = "";
+		
+		// Do-While para hacer hasta que se introduzca 5 en el menú.
+		do {
+			
+			// Menu de opciones...
+			System.out.println("\n======================\n"
+							 + "   MENÚ DE OPCIONES"
+							 + "\n======================\n"
+							 + "A. Listado.\r\n"
+							 + "B. Nuevo Pedido.\r\n"
+							 + "C. Pizza Servida.\r\n"
+							 + "D. Salir.\r\n"
+							 + "----------------------\n");
+			System.out.print("Opción: ");
+			opcion = sc.next().toUpperCase();
+			sc.nextLine();
+			
+			switch (opcion) {
+			
+				case "A" -> {
+					
+					if(crud.listadoPizzas().isEmpty()) {
+				        System.err.println("❌ No hay artículos registrados ❌");
+				        
+					} else {
+						System.out.println("\n===========================");
+						System.out.println("🗂 Listado de Pizzas 🗂");
+						System.out.println("===========================");
+				        for (Pizza pizza : crud.listadoPizzas()) {
+				            System.out.println("🔸 " + pizza);
+				            System.out.println("---------------------");
+				        }
+					}
+				}
+				
+				case "B" -> {
+					
+					System.out.println("Nuevo Pedido");
+					System.out.print("Elige Tamaño (Mediana - Grande): ");
+					tamaño = sc.next();
+					System.out.print("Elige Tipo (Margarita, CuatroQuesos, Funghi): ");
+					tipo = sc.next();
+					
+					if(crud.nuevoPedido(codigo, tamaño, tipo)) {
+						++codigo;
+						System.out.println("Pedido Registrado Correctamente");
+					} else {
+						System.err.println("Error: Revisa que el Tamaño y Tipo Elegido se Correspondan con los Indicados.");
+					}
+					
+				}
+			}
+			
+		}while (!opcion.equals("D"));
+	}
+
+}
